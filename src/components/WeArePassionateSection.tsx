006b4f193f83380efda1da4cf45f21a40069300e
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-03-21 16:49:01 +0100

    fixed margins left either vwport

--- src/components/WeArePassionateSection.tsx
+++ src/components/WeArePassionateSection.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
 
-
 const NewSection = () => {
   return (
-   
-        <section
-        className="text-center w-full relative overflow-hidden flex items-center justify-center"
-        style={{ background: '#15253E', height: '503px' }}
-      >
-      {/* Text Container */}
-      <div className="flex-1 px-4">
-        <div style={{ color: 'white', fontSize: 56, fontWeight: '700' }}>Vi är</div>
-        <div style={{ color: '#33ABBD', fontSize: 56, fontWeight: '700' }}>passionerade</div>
-        <div style={{ width: '100%', color: 'white', fontSize: 20, fontWeight: '400' }}>
+    <section className="text-center w-full h-[503px] relative overflow-hidden flex flex-col lg:flex-row items-center justify-center bg-[#15253E]">
+      
+      {/* Image Container */}
+      <div className="w-full lg:w-1/2 lg:order-2">
+        <img src="/images/imageConvertor1.jpeg" alt="Beskrivande text" className="w-full h-[250px] lg:h-[503px] object-cover" />
+      </div>
+      
+      {/* Text Container - Uppdaterad med responsiva marginaler */}
+      <div className="w-full lg:w-1/2 lg:order-1 px-4 lg:ml-[180px] ml-[20px] md:ml-[40px] flex flex-col text-left justify-center">
+        <h1 className="text-white font-bold text-4xl md:text-5xl lg:text-6xl leading-tight font-century-gothic-pro">Vi är <span className="text-[#33ABBD]">passionerade</span></h1>
+        <p className="text-white text-base md:text-lg lg:text-xl font-century-gothic-pro font-light mt-2">
           Vi är passionerade – det ligger i vårt DNA.<br/>
           Vi är nyfikna på alla våra uppdragsgivares olika branscher, men också på de möjligheter vi med teknikens hjälp kan skapa tillsammans.<br/><br/>
           Vi är alltid lojala mot uppgiften, drar oss inte för att utmana det förväntade och släpper inte greppet förrän vi levererat en lösning vi alla kan vara stolta över
-        </div>
-      </div>
-      
-      {/* Image Container */}
-      <div className="flex-1">
-        <img src="public/images/imageDaniel.jpeg" alt="Beskrivande text" style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} />
+        </p>
       </div>
     </section>
   );
