aa9fc915f71785f38aab8b8e5fc261943be8aad8
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-03-22 11:10:40 +0100

    fixed some styling in PrestigelessSection

--- src/components/PrestigelessSection.tsx
+++ src/components/PrestigelessSection.tsx
@@ -3,18 +3,18 @@ import React from 'react';
 const PrestigelessSection = () => {
   return (
     <section className="text-center w-full relative overflow-hidden flex flex-col items-center justify-center bg-white p-8 sm:p-12 md:p-16 lg:p-20">
-      {/* Text Container*/}
+      {/* Image Container - Lägg denna först i koden och använd "md:order-last" för att flytta den efter texten på skärmar större än "md" */}
+      <div className="w-full max-w-4xl mx-auto md:order-last">
+        <img src="/images/imageConvertor2.png" alt="Beskrivande text" className="w-full h-auto object-contain"/>
+      </div>
+
+      {/* Text Container - Denna kommer att vara först på skärmar större än "md" eftersom bilden använder "md:order-last" */}
       <div className="w-full max-w-4xl mx-auto mb-8 lg:mb-12 text-left">
-      <h1 className="text-black font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-left md:text-center">Vi är <span className="text-[#33ABBD]">prestigelösa</span></h1>
+        <h1 className="text-black font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-left md:text-center mt-5">Vi är <span className="text-[#33ABBD]">prestigelösa</span></h1>
         <p className="text-black text-lg md:text-xl lg:text-2xl font-light mt-4 break-words">
           Vi har ögonen på bollen och vet vad som är viktigt. Och för oss handlar det varken om titlar eller formalia, utan om att skapa de allra bästa förutsättningarna – för våra kunder och för varandra, som kollegor.<br/><br/>
           På Convertor utvecklas vi tillsammans, vi tror på öppenhet och transparens och vi vet att vår allra största tillgång är människorna vi har omkring oss.
         </p>
-      </div>
-
-      {/* Image Container */}
-      <div className="w-full max-w-4xl mx-auto">
-        <img src="/images/imageConvertor2.png" alt="Beskrivande text" className="w-full h-auto object-contain"/>
       </div>
     </section>
   );
