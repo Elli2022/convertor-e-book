6304913d83200eeac7790c12690cf3a323754393
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-03-25 16:34:06 +0100

    fixed elipse in ResultOrientedSection.tsx

--- src/components/ResultOrientedSection.tsx
+++ src/components/ResultOrientedSection.tsx
@@ -5,13 +5,20 @@ const ResultOrientedSection = () => {
     <>
       <section className="text-center w-full relative overflow-hidden flex flex-col lg:flex-row items-center justify-center bg-[#D3E0E5] p-8 sm:p-12 md:p-16 lg:p-20 group">
         
+
+      {/* Ellipse (Circle) */}
+<div className="absolute left-0 bottom-0 w-[65vw] h-[65vw] md:w-[25vw] md:h-[25vw] bg-white rounded-full"
+     style={{ transform: 'translateX(-60%)' }} />
+
+
+
         {/* Image Container */}
         <div className="w-full lg:w-1/2 flex justify-center lg:justify-end" style={{ maxWidth: '503px' }}>
-          <img src="/images/imageConvertor3.png" alt="Beskrivande text" className="w-full h-auto lg:h-[503px] lg:w-[503px] object-cover transition-transform duration-300 ease-in-out group-hover:scale-110" />
+          <img src="/images/imageConvertor3.png" alt="Beskrivande text" className="w-full h-auto lg:h-[503px] lg:w-[503px] object-cover transition-transform duration-300 ease-in-out group-hover:scale-110 z-10" />
         </div>
         
         {/* Text Container */}
-        <div className="w-full lg:w-1/2 flex flex-col text-left justify-center lg:ml-14">
+        <div className="w-full lg:w-1/2 flex flex-col text-left justify-center lg:ml-14 z-10">
           <h1 className="text-black font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-tight font-century-gothic-pro mt-5">Vi är <span className="text-[#33ABBD]">resultatdrivna</span></h1>
           <p className="text-black text-base md:text-lg lg:text-xl font-extralight mt-4">
             På Convertor gör vi skillnad, på riktigt. Vi lockar inte med tomma ord och fina löften utan erbjuder alla våra kunder konkreta och mätbara resultat.<br/><br/>
