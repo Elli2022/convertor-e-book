38714842774230fdde6bc2901683f516c646c25f
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-04-04 11:40:42 +0200

    best text layout on section

--- src/components/WeAreDoersSection.tsx
+++ src/components/WeAreDoersSection.tsx
@@ -29,13 +29,14 @@ const WeAreDoersSection = () => {
         {/* Text Container */}
         <div className="z-10 max-w-4xl mx-auto px-4">
           <h1 className="text-white font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-tight font-century-gothic-pro mt-5">
-            Med andra ord: <br />vi är <span className="text-[#33ABBD]">doers.</span>
+            Med andra ord: <br />
+            vi är <span className="text-[#33ABBD]">doers.</span>
           </h1>
-          <p className="text-white text-base md:text-lg lg:text-xl font-extralight mt-4">
-            Vi pratar inte. Vi gör. Vi levererar och vi får folk<br/>
-            att se nya möjligheter. Varje dag, i varje projekt,<br/>
-            anstränger vi oss alltid för att ge det där lilla extra.<br/>
-            Och så har vi himla kul under tiden, förstås.
+          <p className="text-white text-base md:text-lg lg:text-xl font-extralight mt-4 text-left max-w-4xl">
+            Vi pratar inte. Vi gör. Vi levererar och vi får folk att se nya
+            möjligheter. Varje dag, i varje projekt, anstränger vi oss alltid
+            för att ge det där lilla extra. Och så har vi himla kul under tiden,
+            förstås.
           </p>
         </div>
       </section>
