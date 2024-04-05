01351e42e9f573e3aaee7a05440a24c115bf86b1
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-04-05 15:41:19 +0200

    push

--- src/components/PrestigelessSection.tsx
+++ src/components/PrestigelessSection.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 
+
 const PrestigelessSection = () => {
   return (
     <>
       <section className="text-center w-full relative overflow-hidden flex flex-col items-center justify-center bg-white p-8 sm:p-12 md:p-16 lg:p-20 group">
         
         {/* Ellipse (Circle) */}
-        <div className="absolute right-0 top-1/2 w-[50vw] h-[50vw] md:w-[35vw] md:h-[35vw] bg-[#D3E0E5] rounded-full"
+        <div  className="absolute right-0 top-1/2 w-[50vw] h-[50vw] md:w-[35vw] md:h-[35vw] bg-[#D3E0E5] rounded-full"
              style={{ transform: 'translate(25%, -50%)' }} />
 
-        {/* Image Container */}
+        {/* Bildcontainer */}
         <div className="w-full max-w-4xl mx-auto md:order-last z-10">
           <img src="/images/imageConvertor2.png" alt="Beskrivande text" className="w-full h-auto object-contain transition-transform duration-300 ease-in-out group-hover:scale-110" />
         </div>
 
-        {/* Text Container */}
+        {/* Textcontainer */}
         <div className="w-full max-w-4xl mx-auto mb-8 lg:mb-12 text-left z-10">
           <h1 className="text-black font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-left md:text-center mt-5">Vi är <span className="text-[#33ABBD]">prestigelösa</span></h1>
           <p className="text-black text-base md:text-lg lg:text-xl font-extralight mt-4 break-words">
