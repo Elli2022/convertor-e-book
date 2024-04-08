db94d978a2f238564084f591beffb7fe9ad50846
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-04-08 15:38:13 +0200

    added textObserver on NewSection

--- src/components/WeArePassionateSection.tsx
+++ src/components/WeArePassionateSection.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
+"use client"
+import React, { useRef, useEffect } from 'react';
 
-const NewSection = () => {
+const NewSection: React.FC = () => {
+  const textContainerRef = useRef<HTMLDivElement>(null); // Typreferens till textcontainern
+
+  useEffect(() => {
+    const observerOptions: IntersectionObserverInit = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    };
+
+    const textObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('wave-in');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, observerOptions);
+
+    const nodes = textContainerRef.current?.querySelectorAll('h1, p, p > span');
+    nodes?.forEach((node) => {
+      textObserver.observe(node);
+    });
+
+    return () => {
+      if (textObserver) {
+        nodes?.forEach((node) => {
+          textObserver.unobserve(node);
+        });
+      }
+    };
+  }, []);
+
   return (
     <>
       <section className="text-center w-full relative overflow-hidden flex flex-col lg:flex-row items-center justify-center bg-[#15253E] p-8 sm:p-12 md:p-16 lg:p-20">
-        
         {/* Image Container */}
         <div className="w-full lg:w-1/2 lg:order-2 flex justify-center lg:justify-end lg:ml-14 group" style={{ maxWidth: '503px' }}>
           <img src="/images/imageConvertor1.jpeg" alt="Beskrivande text" className="w-full h-auto lg:h-[503px] lg:w-[503px] object-cover transition-transform duration-300 ease-in-out group-hover:scale-110" />
         </div>
         
         {/* Text Container */}
-        <div className="w-full lg:w-1/2 lg:order-1 flex flex-col text-left justify-center">
+        <div ref={textContainerRef} className="w-full lg:w-1/2 lg:order-1 flex flex-col text-left justify-center">
           <h1 className="text-white font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl leading-tight font-century-gothic-pro mt-5">Vi är <span className="text-[#33ABBD]">passionerade</span></h1>
           <p className="text-white text-base md:text-lg lg:text-xl font-extralight mt-4">
             Vi är passionerade – det ligger i vårt DNA.<br/>
