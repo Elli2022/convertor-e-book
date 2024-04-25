4500728e550d99026015cce94af043390043d178
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-04-25 16:19:26 +0200

    test 17

--- src/components/WelcomeSection/WelcomeSection.tsx
+++ src/components/WelcomeSection/WelcomeSection.tsx
@@ -27,7 +27,7 @@ const WelcomeSection: React.FC = () => {
         return true;  // Prevent default to avoid scrolling the page
       }
     } else {
-      if (atTopOfPage || scale > scaleStart) {
+      if ((atTopOfPage && scale > scaleStart) || scale > scaleStart) {
         const newScale = Math.max(scale - scaleIncreaseRate * scrollDelta, scaleStart);
         setScale(newScale);
         if (newScale === scaleStart) {
